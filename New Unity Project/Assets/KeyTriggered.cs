b42990171f4f49fe3229f46f08167444f62dcff2
Edward Yoon <edwardyoon1326@gmail.com>
2018-02-24 17:27:26 -0800

    5:27

--- New Unity Project/Assets/KeyTriggered.cs
+++ New Unity Project/Assets/KeyTriggered.cs
@@ -6,11 +6,22 @@ public class KeyTriggered : MonoBehaviour {
 
     private GameObject collidingObject;
 
-    public void OnTriggerEnter(Collider other)
+    public IEnumerator OnTriggerEnter()
     {
+        collidingObject = GameObject.Find("key_gold");
+        GameObject.FindWithTag("key_gold").layer = 10;
+        yield return new WaitForSeconds(2);
+        GameObject.FindWithTag("key_gold").SetActive(false);
+        Explode();
         Debug.Log("Key is in");
     }
-
+    void Explode()
+    {
+        var exp = GetComponent<ParticleSystem>();
+        exp.Play();
+        Destroy(gameObject);
+    }
+    
     // Update is called once per frame
     void Update()
     {
